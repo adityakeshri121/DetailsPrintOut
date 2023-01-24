d814ed2e0dc9d7843a5944ae8f095e6471525ee1
adityakeshri121 <adityakeshrisam1729@gmail.com>
2023-01-24 18:06:20 +0530

    added tailwind config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>adi</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div className='flex flex-row justify-evenly'>
+        <div>adi</div>
+        <div>tya</div>
+      </div>
     </div>
   );
 }
